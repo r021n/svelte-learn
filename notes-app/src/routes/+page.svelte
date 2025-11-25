cb25c85aef78049c2bc1ab07f0f89dfe246fe6af
r021n <rozinabdul25@gmail.com>
2025-11-26 06:34:09 +0700

    feat: replace welcome message with a card component for improved UI presentation

--- notes-app/src/routes/+page.svelte
+++ notes-app/src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index';
+	import { Card, CardContent, CardHeader, CardTitle } from '$lib/components/ui/card/index';
+</script>
+
+<div class="flex h-screen items-center justify-center bg-slate-50">
+	<Card class="w-[350px]">
+		<CardHeader>
+			<CardTitle>Setup Berhasil</CardTitle>
+		</CardHeader>
+		<CardContent>
+			<p class="mb-4 text-sm text-muted-foreground">
+				Sveltekit + Tailwind v4 + Shadcn siap digunakan
+			</p>
+			<Button class="w-full">Klik Saya</Button>
+		</CardContent>
+	</Card>
+</div>
+
+<style lang="postcss">
+	@reference "tailwindcss";
+</style>
